8a4b989539d8ed88587bf5726df51d59ad7999a5
Tensys57 <tensys57@gmail.com>
2023-06-06 12:39:11 +0300

    waiting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,17 @@
-import { Component } from 'react';
+import { useState } from "react";
+import { useEffect } from 'react';
 
 import { Section } from './Section/Section';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+const App =()=> {
+  const [good,setGood] = useState (0) ;
+   const [neutral,setNeutral] = useState (0) ;
+   const [bad,setBad] = useState (0) ;
+
   onClickHandler = option => {
-    this.setState(prevState => ({ [option]: prevState[option] + 1 }));
+  prevState => prevState[option] + 1;
   };
 
   countTotalFeedback = () => {
@@ -24,10 +24,10 @@ export class App extends Component {
     return total ? Math.round((good / total) * 100) : 0;
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const buttonSet = Object.keys(this.state);
-    return (
+   
+  
+  const buttonSet = Object.keys(this.state);
+   const markup = (
       <div
         style={{
           height: '100vh',
